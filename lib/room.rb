468e0e2c27166f1d3155e80505e89df18c5e014f
Matthew Lim <matlim200@gmail.com>
2023-05-18 08:25:46 -0700

    Initial commit

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,14 @@
 class Room
 
-  def initialize
+  attr_reader :category, :length, :width
 
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+  end
+
+  def area
+    @length * @width.to_i
   end
 end
